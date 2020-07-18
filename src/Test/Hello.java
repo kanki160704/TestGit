6a4c02a3833c14a57a362ab667c7472d5409bdd1
kanki <2050360276@qq.com>
2020-07-18 19:25:34 -0400

    测试

--- src/Test/Hello.java
+++ src/Test/Hello.java
@@ -2,8 +2,7 @@ package Test;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("222");
-        System.out.println("222");
+        System.out.println("aaa");
+        System.out.println("bbb");
     }
 }
